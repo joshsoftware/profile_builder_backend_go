971c0ec3863ef701027b57e681698d4418991cba
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-16 00:13:34 +0530

    create user struct

--- internal/pkg/dto/profile.go
+++ internal/pkg/dto/profile.go
@@ -7,23 +7,27 @@ import (
 	"github.com/joshsoftware/profile_builder_backend_go/internal/pkg/constants"
 )
 
-type CreateProfileRequest struct{
+type CreateProfileRequest struct {
 	Profile Profile `json:"profile"`
 }
 
-type Profile struct{
-	Name string `json:"name"`
+type User struct {
+	ID    int64  `json:"id"`
 	Email string `json:"email"`
-	Gender string `json:"gender"`
-	Mobile string `json:"mobile"`
-	Designation string `json:"designation"`
-	Description string `json:"description"`
-	Title string `json:"title"`
-	YearsOfExperience float64 `json:"years_of_experience"`
+}
+type Profile struct {
+	Name              string   `json:"name"`
+	Email             string   `json:"email"`
+	Gender            string   `json:"gender"`
+	Mobile            string   `json:"mobile"`
+	Designation       string   `json:"designation"`
+	Description       string   `json:"description"`
+	Title             string   `json:"title"`
+	YearsOfExperience float64  `json:"years_of_experience"`
 	PrimarySkills     []string `json:"primary_skills"`
 	SecondarySkills   []string `json:"secondary_skills"`
-	GithubLink string `json:"github_link"`
-	LinkedinLink string `json:"linkedin_link"`
+	GithubLink        string   `json:"github_link"`
+	LinkedinLink      string   `json:"linkedin_link"`
 }
 
 func (req *CreateProfileRequest) Validate() error {
