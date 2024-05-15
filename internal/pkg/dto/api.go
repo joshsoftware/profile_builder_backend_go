23639948d0acb6ce24fbac04cb84d9b59a1c26ca
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-16 00:15:31 +0530

    create user login request and login response struct

--- internal/pkg/dto/api.go
+++ internal/pkg/dto/api.go
@@ -3,3 +3,16 @@ package dto
 type MessageResponse struct {
 	Message string `json:"message"`
 }
+type UserLoginRequest struct {
+	AccessToken string `json:"access_token"`
+}
+
+type UserLoginResponse struct {
+	Message    string `json:"message"`
+	Token      string `json:"token"`
+	StatusCode int    `json:"status_code"`
+}
+
+type UserInfo struct {
+	Email string `json:"email"`
+}
