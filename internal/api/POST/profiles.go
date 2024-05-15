297c156ca251154304067dcd3a6485209c6e3e60
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-16 00:14:31 +0530

    create login handler

--- internal/api/POST/profiles.go
+++ internal/api/POST/profiles.go
@@ -1,8 +1,13 @@
 package post
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/joshsoftware/profile_builder_backend_go/internal/app/profile"
 	"github.com/joshsoftware/profile_builder_backend_go/internal/pkg/dto"
@@ -32,5 +37,62 @@ func CreateProfileHandler(profileSvc profile.Service, ctx context.Context) func(
 		middleware.SuccessResponse(w, http.StatusCreated, dto.MessageResponse{
 			Message: "Basic info added successfully",
 		})
+	}
+}
+
+func Login(profileSvc profile.Service, ctx context.Context) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		req, err := decodeUserLoginRequest(r)
+		if err != nil {
+			middleware.ErrorResponse(w, http.StatusBadRequest, err)
+			return
+		}
+
+		fmt.Println("Access token: ", req.AccessToken)
+
+		serverRequest, err := http.NewRequest("GET", os.Getenv("GOOGLE_USER_INFO_URL"), nil)
+		if err != nil {
+			middleware.ErrorResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		fmt.Println("service request : ", serverRequest)
+		serverRequest.Header.Set("Authorization", "Bearer "+req.AccessToken)
+
+		resp, err := http.DefaultClient.Do(serverRequest)
+		if err != nil {
+			middleware.ErrorResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			middleware.ErrorResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		fmt.Println("response body : ", string(body))
+
+		var userInfo dto.UserInfo
+		if err := json.NewDecoder(bytes.NewReader(body)).Decode(&userInfo); err != nil {
+			middleware.ErrorResponse(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		token, err := profileSvc.GenerateLoginToken(ctx, userInfo.Email)
+		if err != nil {
+			middleware.ErrorResponse(w, http.StatusUnauthorized, err)
+			return
+		}
+		w.Header().Set("Authorization", "Bearer "+token)
+
+		loginResp := dto.UserLoginResponse{
+			Message:    "Login successful",
+			Token:      token,
+			StatusCode: http.StatusOK,
+		}
+
+		middleware.SuccessResponse(w, http.StatusOK, loginResp)
 	}
 }
