e249ec06d0c1004db13423441ec502997f6158ca
Abhishek-Jain-1925 <jainabhishek1925@gmail.com>
2024-05-19 13:39:25 +0530

    Added CORS

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/joshsoftware/profile_builder_backend_go/internal/api"
 	"github.com/joshsoftware/profile_builder_backend_go/internal/app"
 	"github.com/joshsoftware/profile_builder_backend_go/internal/repository"
+	"github.com/rs/cors"
 	"go.uber.org/zap"
 )
 
@@ -44,7 +45,15 @@ func main() {
 	//Initializaing Router
 	router := api.NewRouter(ctx, services)
 
-	err = http.ListenAndServe("localhost:1925", router)
+	// CORS middleware
+    cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowedHeaders:   []string{"*"},
+	})
+
+	err = http.ListenAndServe("localhost:1925", cors.Handler(router))
 	if err != nil {
 		logger.Error("Error Starting Server  : ", zap.Error(err))
 		return
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/jackc/pgx/v5 v5.5.5
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/cors v1.11.0
 	github.com/stretchr/testify v1.8.1
 	go.uber.org/zap v1.27.0
 )
