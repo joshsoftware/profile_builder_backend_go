e026957f5f61415552e60e20211fad137ca4a2ea
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-28 10:23:54 +0530

    add constant error in errors file

--- internal/pkg/errors/errors.go
+++ internal/pkg/errors/errors.go
@@ -9,12 +9,15 @@ var (
 	ErrInvalidID        = errors.New("invalid id")
 	ErrEmptyPayload     = errors.New("empty payload array")
 	ErrSecretKey        = errors.New("secret key not found")
+	ErrEmailNotFound    = errors.New("email not found")
 )
 
 // Profile Related variables
 var (
-	ErrInvalidFormat      = errors.New("invalid request format")
-	ErrInvalidRequestData = errors.New("invalid request data")
+	ErrInvalidFormat          = errors.New("invalid request format")
+	ErrInvalidRequestData     = errors.New("invalid request data")
+	ErrHTTPRequestFailed      = errors.New("failed to perform HTTP request")
+	ErrReadResponseBodyFailed = errors.New("failed to read response body")
 )
 
 // DB Related variables
