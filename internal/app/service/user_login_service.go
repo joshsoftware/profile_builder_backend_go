181ca8c650e2f52b0d216efe2c5b15667e886347
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-28 10:23:36 +0530

    move create token and createClaims into pkg folder

--- internal/app/service/user_login_service.go
+++ internal/app/service/user_login_service.go
@@ -3,11 +3,7 @@ package service
 import (
 	"context"
 
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt"
-	"github.com/joshsoftware/profile_builder_backend_go/internal/pkg/errors"
+	jwttoken "github.com/joshsoftware/profile_builder_backend_go/internal/pkg/jwt_token"
 )
 
 type UserLoginServive interface {
@@ -15,45 +11,15 @@ type UserLoginServive interface {
 }
 
 func (userService *service) GenerateLoginToken(ctx context.Context, email string) (string, error) {
-	if len((email)) == 0 {
-		return "", errors.ErrEmptyPayload
-	}
-
 	userId, err := userService.UserLoginRepo.GetUserIdByEmail(ctx, email)
 	if err != nil || userId == 0 {
 		return "", err
 	}
 
-	token, err := userService.createToken(userId, email)
+	token, err := jwttoken.CreateToken(userId, email)
 	if err != nil {
 		return "", err
 	}
 	return token, nil
 
-}
-
-func (userLoginService *service) createToken(userId int64, email string) (string, error) {
-	secretKey := os.Getenv("SECRET_KEY")
-
-	if secretKey == "" {
-		return "", errors.ErrSecretKey
-	}
-
-	claims := userLoginService.createClaims(userId, email)
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	tokenString, err := token.SignedString([]byte(secretKey))
-	if err != nil {
-		return "", err
-	}
-	return tokenString, nil
-}
-
-func (profileSvc *service) createClaims(userId int64, email string) jwt.MapClaims {
-	return jwt.MapClaims{
-		"authorised": true,
-		"userId":     userId,
-		"email":      email,
-		"exp":        time.Now().Add(time.Hour * 72).Unix(),
-	}
 }
