691a39e8481bd3600d6e29b076cff9db1bf42f1f
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-29 18:10:21 +0530

    add repo layer dependencies to service layer

--- cmd/main.go
+++ cmd/main.go
@@ -44,8 +44,18 @@ func main() {
 	}
 	fmt.Println("Connected to Database!")
 
+	var repodeps = service.RepoDeps{
+		UserLoginDeps:   repository.NewUserLoginRepo(db),
+		ProfileDeps:     repository.NewProfileRepo(db),
+		EducationDeps:   repository.NewEducationRepo(db),
+		ExperienceDeps:  repository.NewExperienceRepo(db),
+		ProjectDeps:     repository.NewProjectRepo(db),
+		CertificateDeps: repository.NewCertificateRepo(db),
+		AchievementDeps: repository.NewAchievementRepo(db),
+	}
+
 	//Initializing Services
-	services := service.NewServices(db)
+	services := service.NewServices(repodeps)
 
 	//Initializing Router
 	router := api.NewRouter(ctx, services)
