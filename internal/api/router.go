3c32849e1299370177f938189384acbfb3c8b1af
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-31 13:52:34 +0530

    add middleware to all routers

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joshsoftware/profile_builder_backend_go/internal/api/handler"
 	"github.com/joshsoftware/profile_builder_backend_go/internal/app/service"
+	"github.com/joshsoftware/profile_builder_backend_go/internal/pkg/middleware"
 )
 
 // NewRouter returns a object that contains all routes of application
@@ -15,26 +16,28 @@ func NewRouter(ctx context.Context, svc service.Service) *mux.Router {
 
 	// user login router
 	router.HandleFunc("/login", handler.Login(ctx, svc)).Methods(http.MethodPost)
-	subrouter := router.PathPrefix("/profiles").Subrouter()
+
+	profileSubrouter := router.PathPrefix("").Subrouter()
+	profileSubrouter.Use(middleware.AuthMiddleware)
 
 	//POST APIs
-	router.HandleFunc("/profiles", handler.CreateProfileHandler(ctx, svc)).Methods(http.MethodPost)
-	subrouter.HandleFunc("/educations", handler.CreateEducationHandler(ctx, svc)).Methods(http.MethodPost)
-	subrouter.HandleFunc("/projects", handler.CreateProjectHandler(ctx, svc)).Methods(http.MethodPost)
-	subrouter.HandleFunc("/experiences", handler.CreateExperienceHandler(ctx, svc)).Methods(http.MethodPost)
-	subrouter.HandleFunc("/certificates", handler.CreateCertificateHandler(ctx, svc)).Methods(http.MethodPost)
-	subrouter.HandleFunc("/achievements", handler.CreateAchievementHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles", handler.CreateProfileHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/educations", handler.CreateEducationHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles/{project_id}/projects", handler.CreateProjectHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/experiences", handler.CreateExperienceHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/certificates", handler.CreateCertificateHandler(ctx, svc)).Methods(http.MethodPost)
+	profileSubrouter.HandleFunc("/profiles/{profiles_id}/achievements", handler.CreateAchievementHandler(ctx, svc)).Methods(http.MethodPost)
 
 	//GET APIs
-	router.HandleFunc("/list_profiles", handler.ProfileListHandler(ctx, svc)).Methods(http.MethodGet)
-	router.HandleFunc("/profiles", handler.GetProfileHandler(ctx, svc)).Methods(http.MethodGet)
-	subrouter.HandleFunc("/{profile_id}/educations", handler.GetEducationHandler(ctx, svc)).Methods(http.MethodGet)
-	subrouter.HandleFunc("/{profile_id}/projects", handler.GetProjectHandler(ctx, svc)).Methods(http.MethodGet)
-	subrouter.HandleFunc("/{profile_id}/experiences", handler.GetExperienceHandler(ctx, svc)).Methods(http.MethodGet)
+	profileSubrouter.HandleFunc("/list_profiles", handler.ProfileListHandler(ctx, svc)).Methods(http.MethodGet)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}", handler.GetProfileHandler(ctx, svc)).Methods(http.MethodGet)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/educations", handler.GetEducationHandler(ctx, svc)).Methods(http.MethodGet)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/projects", handler.GetProjectHandler(ctx, svc)).Methods(http.MethodGet)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/experiences", handler.GetExperienceHandler(ctx, svc)).Methods(http.MethodGet)
 
-	//PUT APIs
-	router.HandleFunc("/profiles", handler.UpdateProfileHandler(ctx, svc)).Methods(http.MethodPut)
-	subrouter.HandleFunc("/{profile_id}/educations/{id}", handler.UpdateEducationHandler(ctx, svc)).Methods(http.MethodPut)
+	// PUT APIs
+	profileSubrouter.HandleFunc("/profiles/{profile_id}", handler.UpdateProfileHandler(ctx, svc)).Methods(http.MethodPut)
+	profileSubrouter.HandleFunc("/profiles/{profile_id}/educations/{id}", handler.UpdateEducationHandler(ctx, svc)).Methods(http.MethodPut)
 
 	return router
 }
