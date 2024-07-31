7fbb2d2fdc1afd1609abbfb224855c63723a0dde
Saurabh Puri <purisaurabh2885@gmail.com>
2024-07-31 17:14:52 +0530

    add migration

--- internal/db/migrations/000003_add_invitations_table.up.sql
+++ internal/db/migrations/000003_add_invitations_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS invitations (
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	profile_id INT NOT NULL,
-	profile_complete INT NOT NULL CHECK (profile_complete BETWEEN 0 AND 1),
-	created_at DATE NOT NULL DEFAULT CURRENT_DATE,
-	updated_at DATE NOT NULL DEFAULT CURRENT_DATE,
+	is_profile_complete INT NOT NULL CHECK (is_profile_complete BETWEEN 0 AND 1),
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	created_by_id INT NOT NULL,
 	updated_by_id INT NOT NULL,
 	
@@ -14,4 +14,4 @@ CREATE TABLE IF NOT EXISTS invitations (
 );
 -- check uniique constraint for profile_id and profile_complete
 CREATE UNIQUE INDEX unique_profile_complete_zero ON invitations (profile_id) 
-WHERE profile_complete = 0;
+WHERE is_profile_complete = 0;
